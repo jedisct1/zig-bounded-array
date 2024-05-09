0650c39d0aa3dba3c2ad4cc86a35db1387b79988
Frank Denis <github@pureftpd.org>
2024-05-09 21:55:34 +0200

    Update for Zig 0.13

--- build.zig
+++ build.zig
@@ -5,11 +5,11 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     _ = b.addModule("bounded_array", .{
-        .root_source_file = .{ .path = "src/bounded_array.zig" },
+        .root_source_file = b.path("src/bounded_array.zig"),
     });
 
     const main_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/bounded_array.zig" },
+        .root_source_file = b.path("src/bounded_array.zig"),
         .target = target,
         .optimize = optimize,
     });
